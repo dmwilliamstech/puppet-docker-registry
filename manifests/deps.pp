926da9a8029c94585017e5c17a8f17318942d78f
dmwilliams.tech@gmail.com <dmwilliams.tech@gmail.com>
2014-03-20 17:49:23 -0400

    Initial commit

---
+++ manifests/deps.pp
@@ -0,0 +1,34 @@
+class docker-registry::deps{
+    if ! defined(Package['python-devel']){package{'python-devel':}}
+    if ! defined(Package['libevent-devel']){package{'libevent-devel':}}
+    if ! defined(Package['openssl-devel']){package{'openssl-devel':}}
+    if ! defined(Package['wget']){package{'wget':}}
+    if ! defined(Package['python-setuptools']){package{'python-setuptools':}}
+    if ! defined(Package['python-gevent']){package{'python-gevent':}}
+    if ! defined(Package['python-gunicorn']){package{'python-gunicorn':}}
+    if ! defined(Package['python-flask']){package{'python-flask':}}
+    if ! defined(Package['python-werkzeug']){package{'python-werkzeug':}}
+    if ! defined(Package['python-urllib3']){package{'python-urllib3':}}
+    if ! defined(Package['python-six']){package{'python-six':}}
+    if ! defined(Package['python-ordereddict']){package{'python-ordereddict':}}
+    if ! defined(Package['python-chardet']){package{'python-chardet':}}
+    if ! defined(Package['python-backports-ssl_match_hostname']){package{'python-backports-ssl_match_hostname':}}
+    if ! defined(Package['python-backports']){package{'python-backports':}}
+    if ! defined(Package['python-requests']){package{'python-requests':}}
+    if ! defined(Package['python-redis']){package{'python-redis':}}
+    if ! defined(Package['python-blinker']){package{'python-blinker':}}
+    if ! defined(Package['python-yaml']){package{'python-yaml':}}
+    if ! defined(Package['python-simplejson']){package{'python-simplejson':}}
+    if ! defined(Package['python-backports-lzma']){package{'python-backports-lzma':}}
+    if ! defined(Package['pyliblzma']){package{'pyliblzma':}}
+    if ! defined(Package['Jinja2']){package{'Jinja2':}}
+
+    exec{'get_latest_pip':
+        path =>['/usr/bin', '/bin', '/usr/local/bin', '/usr/sbin'],
+        cwd=>'/usr/local/share',
+        command => "wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py --no-check-certificate",
+        creates=>'/usr/local/share/get-pip.py',
+        provider=>'shell',
+        logoutput => true,
+    }
+}
---
+++ manifests/init.pp
@@ -0,0 +1,39 @@
+class docker-registry($port='8000', $IP='0.0.0.0'){
+    include 'docker-registry::deps'
+        
+  if ! defined(Package['git']){package{'git':}}
+
+  exec{'get_latest_stable_docker_registry':
+            path =>['/usr/bin', '/bin', '/usr/local/bin', '/usr/sbin'],
+            cwd=>'/usr/local/share',
+            command => "git clone https://github.com/dotcloud/docker-registry.git",
+            creates=>'/usr/local/share/docker-registry',
+            provider=>'shell',
+            logoutput => true,
+                creates=>'/usr/local/share/docker-registry'
+        }->
+        exec{'copy_config_file':
+          path =>['/usr/bin', '/bin', '/usr/local/bin', '/usr/sbin'],
+          cwd=>'/usr/local/share/docker-registry',
+          command=>'cp config_sample.yml config.yml',
+          provider=>'shell',
+          creates=>'/usr/local/share/docker-registry/config.yml',
+            
+        }->
+            # create a directory      
+        file { '/tmp/registry':
+          ensure => "directory",
+          mode=> 777
+        }->
+        exec{'pip_install_requirements':
+          path =>['/usr/bin', '/bin', '/usr/local/bin', '/usr/sbin'],
+          provider=>'shell',
+          cwd=>'/usr/local/share/docker-registry',
+          command=>'pip install -r requirements.txt'
+              }->
+          exec{'start_registry':
+            path =>['/usr/bin', '/bin', '/usr/local/bin', '/usr/sbin'],
+            provider=>'shell',
+            command=>"gunicorn --access-logfile - --debug -k gevent -b $IP:$port -w 1 wsgi:application"
+        }
+}
