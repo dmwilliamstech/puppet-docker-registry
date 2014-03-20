d672ab5b6f3d260060233916294391d32e4fce04
dmwilliams.tech@gmail.com <dmwilliams.tech@gmail.com>
2014-03-20 18:07:13 -0400

    Ensure installations

--- manifests/deps.pp
+++ manifests/deps.pp
@@ -1,27 +1,27 @@
 class docker-registry::deps{
-    if ! defined(Package['python-devel']){package{'python-devel':}}
-    if ! defined(Package['libevent-devel']){package{'libevent-devel':}}
-    if ! defined(Package['openssl-devel']){package{'openssl-devel':}}
-    if ! defined(Package['wget']){package{'wget':}}
-    if ! defined(Package['python-setuptools']){package{'python-setuptools':}}
-    if ! defined(Package['python-gevent']){package{'python-gevent':}}
-    if ! defined(Package['python-gunicorn']){package{'python-gunicorn':}}
-    if ! defined(Package['python-flask']){package{'python-flask':}}
-    if ! defined(Package['python-werkzeug']){package{'python-werkzeug':}}
-    if ! defined(Package['python-urllib3']){package{'python-urllib3':}}
-    if ! defined(Package['python-six']){package{'python-six':}}
-    if ! defined(Package['python-ordereddict']){package{'python-ordereddict':}}
-    if ! defined(Package['python-chardet']){package{'python-chardet':}}
-    if ! defined(Package['python-backports-ssl_match_hostname']){package{'python-backports-ssl_match_hostname':}}
-    if ! defined(Package['python-backports']){package{'python-backports':}}
-    if ! defined(Package['python-requests']){package{'python-requests':}}
-    if ! defined(Package['python-redis']){package{'python-redis':}}
-    if ! defined(Package['python-blinker']){package{'python-blinker':}}
-    if ! defined(Package['python-yaml']){package{'python-yaml':}}
-    if ! defined(Package['python-simplejson']){package{'python-simplejson':}}
-    if ! defined(Package['python-backports-lzma']){package{'python-backports-lzma':}}
-    if ! defined(Package['pyliblzma']){package{'pyliblzma':}}
-    if ! defined(Package['Jinja2']){package{'Jinja2':}}
+    if ! defined(Package['python-devel']){package{'python-devel':ensure=>installed}}
+    if ! defined(Package['libevent-devel']){package{'libevent-devel':ensure=>installed}}
+    if ! defined(Package['openssl-devel']){package{'openssl-devel':ensure=>installed}}
+    if ! defined(Package['wget']){package{'wget':ensure=>installed}}
+    if ! defined(Package['python-setuptools']){package{'python-setuptools':ensure=>installed}}
+    if ! defined(Package['python-gevent']){package{'python-gevent':ensure=>installed}}
+    if ! defined(Package['python-gunicorn']){package{'python-gunicorn':ensure=>installed}}
+    if ! defined(Package['python-flask']){package{'python-flask':ensure=>installed}}
+    if ! defined(Package['python-werkzeug']){package{'python-werkzeug':ensure=>installed}}
+    if ! defined(Package['python-urllib3']){package{'python-urllib3':ensure=>installed}}
+    if ! defined(Package['python-six']){package{'python-six':ensure=>installed}}
+    if ! defined(Package['python-ordereddict']){package{'python-ordereddict':ensure=>installed}}
+    if ! defined(Package['python-chardet']){package{'python-chardet':ensure=>installed}}
+    if ! defined(Package['python-backports-ssl_match_hostname']){package{'python-backports-ssl_match_hostname':ensure=>installed}}
+    if ! defined(Package['python-backports']){package{'python-backports':ensure=>installed}}
+    if ! defined(Package['python-requests']){package{'python-requests':ensure=>installed}}
+    if ! defined(Package['python-redis']){package{'python-redis':ensure=>installed}}
+    if ! defined(Package['python-blinker']){package{'python-blinker':ensure=>installed}}
+    #if ! defined(Package['python-yaml']){package{'python-yaml':}}
+    if ! defined(Package['python-simplejson']){package{'python-simplejson':ensure=>installed}}
+    if ! defined(Package['python-backports-lzma']){package{'python-backports-lzma':ensure=>installed}}
+    if ! defined(Package['pyliblzma']){package{'pyliblzma':ensure=>installed}}
+
         exec{'easy_install_jinja2':
         path =>['/usr/bin', '/bin', '/usr/local/bin', '/usr/sbin'],
         command=>'easy_install Jinja2',
